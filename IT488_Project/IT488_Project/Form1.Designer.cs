3bdcd6533e706bdcf75628bddaf339dba483482b
Celena Williams <celena.c.williams@gmail.com>
2021-06-09 08:03:35 -0400

    Add files via upload

---
+++ IT488_Project/IT488_Project/Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace IT488_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.categorySelect = new System.Windows.Forms.Label();
+            this.recipeSelect = new System.Windows.Forms.Label();
+            this.recipeLabel = new System.Windows.Forms.Label();
+            this.ingredientsBox = new System.Windows.Forms.RichTextBox();
+            this.caloriesLabel = new System.Windows.Forms.Label();
+            this.carbsLabel = new System.Windows.Forms.Label();
+            this.fatLabel = new System.Windows.Forms.Label();
+            this.proteinLabel = new System.Windows.Forms.Label();
+            this.calOutput = new System.Windows.Forms.Label();
+            this.carbOutput = new System.Windows.Forms.Label();
+            this.fatOutput = new System.Windows.Forms.Label();
+            this.proOutput = new System.Windows.Forms.Label();
+            this.categoryBox = new System.Windows.Forms.ComboBox();
+            this.recipeBox = new System.Windows.Forms.ComboBox();
+            this.instructionsBox = new System.Windows.Forms.RichTextBox();
+            this.currentLabel = new System.Windows.Forms.Label();
+            this.goalLabel = new System.Windows.Forms.Label();
+            this.ORLabel = new System.Windows.Forms.Label();
+            this.currentBox = new System.Windows.Forms.TextBox();
+            this.goalBox = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.displayBox = new System.Windows.Forms.RichTextBox();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.timeBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // categorySelect
+            // 
+            this.categorySelect.AutoSize = true;
+            this.categorySelect.Location = new System.Drawing.Point(47, 13);
+            this.categorySelect.Name = "categorySelect";
+            this.categorySelect.Size = new System.Drawing.Size(136, 13);
+            this.categorySelect.TabIndex = 2;
+            this.categorySelect.Text = "First, Select a Dietary Need";
+            // 
+            // recipeSelect
+            // 
+            this.recipeSelect.AutoSize = true;
+            this.recipeSelect.Location = new System.Drawing.Point(235, 13);
+            this.recipeSelect.Name = "recipeSelect";
+            this.recipeSelect.Size = new System.Drawing.Size(114, 13);
+            this.recipeSelect.TabIndex = 3;
+            this.recipeSelect.Text = "Then, Select a Recipe";
+            // 
+            // recipeLabel
+            // 
+            this.recipeLabel.AutoSize = true;
+            this.recipeLabel.Location = new System.Drawing.Point(47, 97);
+            this.recipeLabel.Name = "recipeLabel";
+            this.recipeLabel.Size = new System.Drawing.Size(0, 13);
+            this.recipeLabel.TabIndex = 5;
+            // 
+            // ingredientsBox
+            // 
+            this.ingredientsBox.Enabled = false;
+            this.ingredientsBox.Location = new System.Drawing.Point(47, 153);
+            this.ingredientsBox.Name = "ingredientsBox";
+            this.ingredientsBox.Size = new System.Drawing.Size(190, 368);
+            this.ingredientsBox.TabIndex = 6;
+            this.ingredientsBox.Text = "";
+            // 
+            // caloriesLabel
+            // 
+            this.caloriesLabel.AutoSize = true;
+            this.caloriesLabel.Location = new System.Drawing.Point(269, 121);
+            this.caloriesLabel.Name = "caloriesLabel";
+            this.caloriesLabel.Size = new System.Drawing.Size(44, 13);
+            this.caloriesLabel.TabIndex = 7;
+            this.caloriesLabel.Text = "Calories";
+            // 
+            // carbsLabel
+            // 
+            this.carbsLabel.AutoSize = true;
+            this.carbsLabel.Location = new System.Drawing.Point(387, 121);
+            this.carbsLabel.Name = "carbsLabel";
+            this.carbsLabel.Size = new System.Drawing.Size(34, 13);
+            this.carbsLabel.TabIndex = 8;
+            this.carbsLabel.Text = "Carbs";
+            // 
+            // fatLabel
+            // 
+            this.fatLabel.AutoSize = true;
+            this.fatLabel.Location = new System.Drawing.Point(488, 121);
+            this.fatLabel.Name = "fatLabel";
+            this.fatLabel.Size = new System.Drawing.Size(22, 13);
+            this.fatLabel.TabIndex = 9;
+            this.fatLabel.Text = "Fat";
+            // 
+            // proteinLabel
+            // 
+            this.proteinLabel.AutoSize = true;
+            this.proteinLabel.Location = new System.Drawing.Point(594, 121);
+            this.proteinLabel.Name = "proteinLabel";
+            this.proteinLabel.Size = new System.Drawing.Size(40, 13);
+            this.proteinLabel.TabIndex = 10;
+            this.proteinLabel.Text = "Protein";
+            // 
+            // calOutput
+            // 
+            this.calOutput.AutoSize = true;
+            this.calOutput.Location = new System.Drawing.Point(320, 121);
+            this.calOutput.Name = "calOutput";
+            this.calOutput.Size = new System.Drawing.Size(0, 13);
+            this.calOutput.TabIndex = 11;
+            // 
+            // carbOutput
+            // 
+            this.carbOutput.AutoSize = true;
+            this.carbOutput.Location = new System.Drawing.Point(428, 121);
+            this.carbOutput.Name = "carbOutput";
+            this.carbOutput.Size = new System.Drawing.Size(0, 13);
+            this.carbOutput.TabIndex = 12;
+            // 
+            // fatOutput
+            // 
+            this.fatOutput.AutoSize = true;
+            this.fatOutput.Location = new System.Drawing.Point(526, 121);
+            this.fatOutput.Name = "fatOutput";
+            this.fatOutput.Size = new System.Drawing.Size(0, 13);
+            this.fatOutput.TabIndex = 13;
+            // 
+            // proOutput
+            // 
+            this.proOutput.AutoSize = true;
+            this.proOutput.Location = new System.Drawing.Point(650, 120);
+            this.proOutput.Name = "proOutput";
+            this.proOutput.Size = new System.Drawing.Size(0, 13);
+            this.proOutput.TabIndex = 14;
+            // 
+            // categoryBox
+            // 
+            this.categoryBox.FormattingEnabled = true;
+            this.categoryBox.Items.AddRange(new object[] {
+            "Keto",
+            "Paleo",
+            "Vegetarian",
+            "SemiVegetarian",
+            "Vegan",
+            "OvoLactoVegetarian",
+            "OvoVegetarian",
+            "LactoVegetarian",
+            "PescoVegetarian",
+            "LactoseIntolerant",
+            "GlutenFree",
+            "SugarFree",
+            "LowSugar"});
+            this.categoryBox.Location = new System.Drawing.Point(47, 41);
+            this.categoryBox.Name = "categoryBox";
+            this.categoryBox.Size = new System.Drawing.Size(144, 21);
+            this.categoryBox.TabIndex = 15;
+            this.categoryBox.SelectedIndexChanged += new System.EventHandler(this.categoryBox_SelectedIndexChanged);
+            // 
+            // recipeBox
+            // 
+            this.recipeBox.Enabled = false;
+            this.recipeBox.FormattingEnabled = true;
+            this.recipeBox.Location = new System.Drawing.Point(238, 41);
+            this.recipeBox.Name = "recipeBox";
+            this.recipeBox.Size = new System.Drawing.Size(377, 21);
+            this.recipeBox.TabIndex = 16;
+            this.recipeBox.SelectedIndexChanged += new System.EventHandler(this.recipeBox_SelectedIndexChanged);
+            // 
+            // instructionsBox
+            // 
+            this.instructionsBox.Enabled = false;
+            this.instructionsBox.Location = new System.Drawing.Point(264, 153);
+            this.instructionsBox.Name = "instructionsBox";
+            this.instructionsBox.Size = new System.Drawing.Size(480, 368);
+            this.instructionsBox.TabIndex = 17;
+            this.instructionsBox.Text = "";
+            // 
+            // currentLabel
+            // 
+            this.currentLabel.AutoSize = true;
+            this.currentLabel.Location = new System.Drawing.Point(802, 31);
+            this.currentLabel.Name = "currentLabel";
+            this.currentLabel.Size = new System.Drawing.Size(106, 13);
+            this.currentLabel.TabIndex = 18;
+            this.currentLabel.Text = "Enter Current Weight";
+            // 
+            // goalLabel
+            // 
+            this.goalLabel.AutoSize = true;
+            this.goalLabel.Location = new System.Drawing.Point(802, 84);
+            this.goalLabel.Name = "goalLabel";
+            this.goalLabel.Size = new System.Drawing.Size(94, 13);
+            this.goalLabel.TabIndex = 19;
+            this.goalLabel.Text = "Enter Goal Weight";
+            // 
+            // ORLabel
+            // 
+            this.ORLabel.AutoSize = true;
+            this.ORLabel.Location = new System.Drawing.Point(697, 31);
+            this.ORLabel.Name = "ORLabel";
+            this.ORLabel.Size = new System.Drawing.Size(45, 13);
+            this.ORLabel.TabIndex = 20;
+            this.ORLabel.Text = "OR First";
+            // 
+            // currentBox
+            // 
+            this.currentBox.Location = new System.Drawing.Point(805, 48);
+            this.currentBox.Name = "currentBox";
+            this.currentBox.Size = new System.Drawing.Size(100, 20);
+            this.currentBox.TabIndex = 23;
+            // 
+            // goalBox
+            // 
+            this.goalBox.Location = new System.Drawing.Point(805, 101);
+            this.goalBox.Name = "goalBox";
+            this.goalBox.Size = new System.Drawing.Size(100, 20);
+            this.goalBox.TabIndex = 24;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(805, 196);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 25;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // displayBox
+            // 
+            this.displayBox.Enabled = false;
+            this.displayBox.Location = new System.Drawing.Point(805, 240);
+            this.displayBox.Name = "displayBox";
+            this.displayBox.Size = new System.Drawing.Size(307, 281);
+            this.displayBox.TabIndex = 26;
+            this.displayBox.Text = "";
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Location = new System.Drawing.Point(802, 141);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(152, 13);
+            this.timeLabel.TabIndex = 27;
+            this.timeLabel.Text = "Enter Your Timeframe (Weeks)";
+            // 
+            // timeBox
+            // 
+            this.timeBox.Location = new System.Drawing.Point(805, 157);
+            this.timeBox.Name = "timeBox";
+            this.timeBox.Size = new System.Drawing.Size(100, 20);
+            this.timeBox.TabIndex = 28;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1163, 564);
+            this.Controls.Add(this.timeBox);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.displayBox);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.goalBox);
+            this.Controls.Add(this.currentBox);
+            this.Controls.Add(this.ORLabel);
+            this.Controls.Add(this.goalLabel);
+            this.Controls.Add(this.currentLabel);
+            this.Controls.Add(this.instructionsBox);
+            this.Controls.Add(this.recipeBox);
+            this.Controls.Add(this.categoryBox);
+            this.Controls.Add(this.proOutput);
+            this.Controls.Add(this.fatOutput);
+            this.Controls.Add(this.carbOutput);
+            this.Controls.Add(this.calOutput);
+            this.Controls.Add(this.proteinLabel);
+            this.Controls.Add(this.fatLabel);
+            this.Controls.Add(this.carbsLabel);
+            this.Controls.Add(this.caloriesLabel);
+            this.Controls.Add(this.ingredientsBox);
+            this.Controls.Add(this.recipeLabel);
+            this.Controls.Add(this.recipeSelect);
+            this.Controls.Add(this.categorySelect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label categorySelect;
+        private System.Windows.Forms.Label recipeSelect;
+        private System.Windows.Forms.Label recipeLabel;
+        private System.Windows.Forms.RichTextBox ingredientsBox;
+        private System.Windows.Forms.Label caloriesLabel;
+        private System.Windows.Forms.Label carbsLabel;
+        private System.Windows.Forms.Label fatLabel;
+        private System.Windows.Forms.Label proteinLabel;
+        private System.Windows.Forms.Label calOutput;
+        private System.Windows.Forms.Label carbOutput;
+        private System.Windows.Forms.Label fatOutput;
+        private System.Windows.Forms.Label proOutput;
+        private System.Windows.Forms.ComboBox categoryBox;
+        private System.Windows.Forms.ComboBox recipeBox;
+        private System.Windows.Forms.RichTextBox instructionsBox;
+        private System.Windows.Forms.Label currentLabel;
+        private System.Windows.Forms.Label goalLabel;
+        private System.Windows.Forms.Label ORLabel;
+        private System.Windows.Forms.TextBox currentBox;
+        private System.Windows.Forms.TextBox goalBox;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.RichTextBox displayBox;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.TextBox timeBox;
+    }
+}
+
---
+++ IT488_Project/IT488_Project/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IT488_Project
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection;
+        SqlCommand command;
+        SqlDataReader reader;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            string server = "192.168.1.130";
+            string database = "SimpleWayToEatHealthy";
+            string username = "HealthyEater";
+            string pass = "Healthy";
+
+            string connectionString = @"Data Source= " + server + "; Initial Catalog= " + database + "; User ID= " + username + "; Password= " + pass;
+
+            connection = new SqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error Logging In.");
+                connection.Close();
+                this.Close();
+            }
+        }
+
+        private void categoryBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            recipeBox.Items.Clear();
+            string categoryName = categoryBox.SelectedItem.ToString();
+
+            selectACategory(categoryName);
+        }
+
+        private void recipeBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string recipeName = recipeBox.SelectedItem.ToString();
+
+            selectARecipe(recipeName);
+        }
+
+        private void selectACategory (string category)
+        {
+            string sqlString = "SELECT RecipeName FROM RecipeTable INNER JOIN CategoriesTable ON RecipeTable.RecipeID = CategoriesTable.RecipeID WHERE "+ category +" = 1;";
+            command = new SqlCommand(sqlString, connection);
+            reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                if (!recipeBox.Items.Contains(reader.GetValue(0)))
+                {
+                    recipeBox.Items.Add(reader.GetValue(0));
+                }
+            }
+            recipeBox.Enabled = true;
+
+            reader.Close();
+            command.Dispose();
+        }
+
+        private void selectARecipe(string recipe)
+        {
+            string sqlString = "SELECT * FROM RecipeTable WHERE RecipeName = '" + recipe + "';";
+            command = new SqlCommand(sqlString, connection);
+            reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                recipeLabel.Text = reader.GetValue(1).ToString();
+
+
+                string ingredients = reader.GetValue(2).ToString();
+                string instructions = reader.GetValue(3).ToString();
+                ingredients = ingredients.Replace('_', '\n');
+                instructions = instructions.Replace('_', '\n');
+                ingredientsBox.Text = "Ingredients:\n" + ingredients;
+                instructionsBox.Text = "Instructions:\n" + instructions;
+
+
+                calOutput.Text = reader.GetValue(4).ToString();
+                carbOutput.Text = reader.GetValue(5).ToString();
+                fatOutput.Text = reader.GetValue(6).ToString();
+                proOutput.Text = reader.GetValue(7).ToString();
+            }
+
+            reader.Close();
+            command.Dispose();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            if (currentBox.Text.Equals(null) || goalBox.Text.Equals(null) || timeBox.Text.Equals(null))
+                MessageBox.Show("Error! Please fiil out all fields!");
+            else
+            {
+                categoryBox.Enabled = false;
+
+                string current = currentBox.Text, goal = goalBox.Text, timeframe = timeBox.Text;
+                int currentWeight = 0, goalWeight = 0, time = 0;
+                int totalChange = 0, totalCalories = 2000;
+                double change = 0, calorieChange = 0, mealCalories = 0;
+
+
+
+                if (System.Text.RegularExpressions.Regex.IsMatch(current, "^[0-9]*$"))
+                {
+                    currentWeight = Convert.ToInt32(current);
+                    displayBox.Text = "Your current weight is " + currentWeight + " lbs.";
+                }
+                else
+                    MessageBox.Show("Error! Enter valid number for your current weight!");
+
+
+                if (System.Text.RegularExpressions.Regex.IsMatch(goal, "^[0-9]*$"))
+                {
+                    goalWeight = Convert.ToInt32(goal);
+                    displayBox.Text += "\nYour goal weight is " + goalWeight + " lbs.";
+                }
+                else
+                    MessageBox.Show("Error! Enter valid number for your goal weight!");
+
+                totalChange = goalWeight - currentWeight;
+
+                displayBox.Text += "\nYour net change is " + totalChange + " lbs.";
+
+                if (System.Text.RegularExpressions.Regex.IsMatch(timeframe, "^[0-9]*$"))
+                {
+                    time = Convert.ToInt32(timeframe);
+                    change = (double) totalChange / (double) time;
+
+                    displayBox.Text += "Your timeframe is " + time + " weeks.";
+                    displayBox.Text += "\nYou will need to change your weight by " + change + " lbs per week.";
+
+                }
+                else
+                    MessageBox.Show("Error! Enter valid number for your timeframe!");
+
+                calorieChange = calorieChangeCalculation(totalCalories, change);
+                mealCalories = calorieChange / 3;
+
+                displayBox.Text += "\nYou will need to consume " + calorieChange + " calories a day (assuming a standard 2,000 calorie diet).";
+                displayBox.Text += "\n\nAlso assuming the standard three meals a day, your general calorie intake for each meal is " + (int) mealCalories + ". You can now view the recipe list.";
+
+                string sqlString = "SELECT RecipeName FROM RecipeTable INNER JOIN CategoriesTable ON RecipeTable.RecipeID = CategoriesTable.RecipeID WHERE Calories <= " + mealCalories + ";";
+
+                command = new SqlCommand(sqlString, connection);
+                reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    if (!recipeBox.Items.Contains(reader.GetValue(0)))
+                    {
+                        recipeBox.Items.Add(reader.GetValue(0));
+                    }
+                }
+                recipeBox.Enabled = true;
+
+                reader.Close();
+                command.Dispose();
+            }
+        }
+
+        private int calorieChangeCalculation (int total, double change)
+        {
+            int weeklyCalories = (int) change * 3000;
+            int dailyCalories = weeklyCalories / 7;
+
+            return total + dailyCalories;
+        }
+    }
+}
